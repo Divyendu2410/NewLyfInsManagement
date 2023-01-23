3ecf88b4fb69fb1e50e8c91dc5a66b987346c44e
Administrator <Administrator@DESKTOP-FUGQNF4>
2023-01-23 14:43:40 +0530

    a new proect

---
+++ src/main/java/com/LfyInsManageDev/testscript/SamClss1.java
@@ -0,0 +1,22 @@
+package com.LfyInsManageDev.testscript;
+
+import org.testng.annotations.Test;
+
+public class SamClss1 {
+	@Test
+	public void simple1() {
+		System.out.println("-Am Groot-");
+	}
+
+	@Test
+	public void simple2() {
+		System.out.println("--Am Am Groot--");
+	}
+	@Test
+	public void simple3() {
+		System.out.println("--Am Am Groot--");
+	}
+	
+
+
+}
---
+++ src/main/java/com/LfyInsManageDev/testscript/SamClss2.java
@@ -0,0 +1,22 @@
+package com.LfyInsManageDev.testscript;
+
+import org.testng.annotations.Test;
+
+public class SamClss2 {
+	@Test
+	public void simple4() {
+		System.out.println("-Am Groot-");
+	}
+
+	@Test
+	public void simple5() {
+		System.out.println("--Am Am Groot--");
+	}
+	@Test
+	public void simple6() {
+		System.out.println("--Am Am Groot--");
+	}
+	
+
+
+}
